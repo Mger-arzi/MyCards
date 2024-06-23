86acd0c5b0676ad79b2b6bd1a912c4cb11020644
Mger-arzi <mger54598@gmail.com>
2024-06-23 10:41:08 +0300

    add

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
-import "./styles/index.scss";
+
+import './styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
 import { App } from './App'
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/700.css";
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
